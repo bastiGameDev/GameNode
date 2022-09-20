da398dd2f7acbb5af107bebaaafa6f79e3bf1426
bastiGameDev <107358763+bastiGameDev@users.noreply.github.com>
2022-09-20 23:56:08 +0300

    Анимация перехода с авторизации

--- GameNodeProject/Assets/Scripts/MainMenuControl.cs
+++ GameNodeProject/Assets/Scripts/MainMenuControl.cs
@@ -7,10 +7,18 @@ public class MainMenuControl : MonoBehaviour
 {
     [SerializeField] private RectTransform loginField;
     [SerializeField] private RectTransform passwordField;
+    [SerializeField] private GameObject panelAuthoriz;
 
     private void Start()
     {
         loginField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
         passwordField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
+    }
+
+    public async void Click()
+    {
+        await panelAuthoriz.transform.DOMoveX(-80, 1f).AsyncWaitForCompletion();
+        
+        panelAuthoriz.SetActive(false);
     }
 }
