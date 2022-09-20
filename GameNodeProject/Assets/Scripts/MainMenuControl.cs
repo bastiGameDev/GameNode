7880de6404b7be81b953c3047826d674e5896bfd
bastiGameDev <107358763+bastiGameDev@users.noreply.github.com>
2022-09-20 23:36:21 +0300

    Реализована визуальная часть регистрации

--- GameNodeProject/Assets/Scripts/MainMenuControl.cs
+++ GameNodeProject/Assets/Scripts/MainMenuControl.cs
@@ -1,14 +1,16 @@
 using System;
 using UnityEngine;
+using DG.Tweening;
 
 
 public class MainMenuControl : MonoBehaviour
 {
-    [SerializeField] private Transform loginField;
-    [SerializeField] private Transform passwordField;
+    [SerializeField] private RectTransform loginField;
+    [SerializeField] private RectTransform passwordField;
 
     private void Start()
     {
-        
+        loginField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
+        passwordField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
     }
 }
