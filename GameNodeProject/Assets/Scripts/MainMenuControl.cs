e22a49161164de353877cdd9229f2a1de904b76a
bastiGameDev <107358763+bastiGameDev@users.noreply.github.com>
2022-09-20 22:51:23 +0300

    Добавлены поля ЛОГИН и ПАРОЛЬ

--- GameNodeProject/Assets/Scripts/MainMenuControl.cs
+++ GameNodeProject/Assets/Scripts/MainMenuControl.cs
@@ -1,17 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+
 
 public class MainMenuControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform loginField;
+    [SerializeField] private Transform passwordField;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
