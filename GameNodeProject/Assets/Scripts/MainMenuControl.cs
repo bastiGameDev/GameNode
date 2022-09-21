79815e56ecfcfe760c64072b2c35f0dd55a210c3
bastiGameDev <107358763+bastiGameDev@users.noreply.github.com>
2022-09-21 18:33:09 +0300

    Save
    Save

--- GameNodeProject/Assets/Scripts/MainMenuControl.cs
+++ GameNodeProject/Assets/Scripts/MainMenuControl.cs
@@ -8,6 +8,7 @@ public class MainMenuControl : MonoBehaviour
     [SerializeField] private RectTransform loginField;
     [SerializeField] private RectTransform passwordField;
     [SerializeField] private GameObject panelAuthoriz;
+    [SerializeField] private GameObject panelMain;
 
     private void Start()
     {
@@ -18,7 +19,24 @@ public class MainMenuControl : MonoBehaviour
     public async void Click()
     {
         await panelAuthoriz.transform.DOMoveX(-80, 1f).SetEase(Ease.InOutQuint).AsyncWaitForCompletion();
-        
         panelAuthoriz.SetActive(false);
+    }
+
+    public async void ButtonMyGamesClick()
+    {
+        await panelMain.transform.DOMoveX(-70f, 1f).SetEase(Ease.InOutQuint).AsyncWaitForCompletion();
+        panelMain.SetActive(false);
+    }
+
+    public async void ButtonMyAccountClick()
+    {
+        await panelMain.transform.DOMoveX(-70f, 1f).SetEase(Ease.InOutQuint).AsyncWaitForCompletion();
+        panelMain.SetActive(false);
+    }
+
+    public async void ButtonMoreGamesClick()
+    { 
+        await panelMain.transform.DOMoveX(-70f, 1f).SetEase(Ease.InOutQuint).AsyncWaitForCompletion();
+        panelMain.SetActive(false);
     }
 }
