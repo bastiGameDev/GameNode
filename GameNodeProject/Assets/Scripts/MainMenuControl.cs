400da1f3231025a28f6ae9f2cd89ca48e8b39a8f
bastiGameDev <107358763+bastiGameDev@users.noreply.github.com>
2022-11-29 10:48:28 +0300

    Удалена анимация
    Удалена анимация перехода с кнопки "Авторизоваться"

--- GameNodeProject/Assets/Scripts/MainMenuControl.cs
+++ GameNodeProject/Assets/Scripts/MainMenuControl.cs
@@ -7,19 +7,19 @@ public class MainMenuControl : MonoBehaviour
 {
     [SerializeField] private RectTransform loginField;
     [SerializeField] private RectTransform passwordField;
-    [SerializeField] private GameObject panelAuthoriz;
+    [SerializeField] private GameObject panelAuthorization;
     [SerializeField] private GameObject panelMain;
 
     private void Start()
     {
-        loginField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
-        passwordField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
+        //loginField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
+        //passwordField.DOMoveX(0f, 1.3f).SetEase(Ease.InOutElastic);
     }
 
     public async void Click()
     {
-        await panelAuthoriz.transform.DOMoveX(-80, 1f).SetEase(Ease.InOutQuint).AsyncWaitForCompletion();
-        panelAuthoriz.SetActive(false);
+        await panelAuthorization.transform.DOMoveX(-80, 1f).SetEase(Ease.InOutQuint).AsyncWaitForCompletion();
+        panelAuthorization.SetActive(false);
     }
 
     public async void ButtonMyGamesClick()
